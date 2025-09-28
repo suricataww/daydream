a57c6bee23bfb19d4b1da406c693620856c9b1bd
suricataww <teodora.maria.mustea@gmail.com>
2025-09-28 07:42:16 +0300

    final

--- area_2fd.gd
+++ area_2fd.gd
@@ -2,14 +2,12 @@ extends Area2D
 
 var entered: bool = false
 
-
-func _on_area_2d_2_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	entered = true
 
-
-func _on_area_2d_2_body_exited(body: Node2D) -> void:
+func _on_body_exited(body: Node2D) -> void:
 	entered = false
-	
+
 func _physics_process(delta: float) -> void:
 	if entered and Input.is_action_just_pressed("enter"):
-		get_tree().change_scene_to_file("res://lvl4.tscn")
+		get_tree().change_scene_to_file("res://ending.tscn")
